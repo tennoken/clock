a7ecb577440664ff79a63e8ae63eb6a41cb00e58
SwiftCloud <tennoken@naver.com>
2019-01-14 16:25:32 +0900

    add background

---
+++ bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    body.prepend(image);
+    image.classList.add("bgImage");
+}
+
+function genRandomNum(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandomNum();
+    paintImage(randomNumber);
+}
+
+init();
